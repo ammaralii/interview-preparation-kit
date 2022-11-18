92cf291d0edcb52f3fa187a4cc59c5f848aa2c7a
Magic <ammar.ali@simplifipay.com>
2022-11-18 12:16:04 +0500

    Solve amazon question 1(buy a perfect set of rice bags)

--- src/main/java/interviews/amazon/hackerrank2/questions/Question1.java
+++ src/main/java/interviews/amazon/hackerrank2/questions/Question1.java
@@ -1,8 +1,70 @@
 package interviews.amazon.hackerrank2.questions;
 
+import java.util.*;
+
 public class Question1 {
-    public static int maxSetSize(int[] riceBags){
-        //Here comes the logic
-        return 0;
+    public static int maxSetSize(int[] riceBags) {
+        return solution2(riceBags);
+    }
+
+    /**
+     * Firstly I solved it using this solution but 7 test cases are failing due to timeout as you see in traverse 2 nested
+     * loops which is causing impact so check the solution2 to check how I optimized it.
+     * <br/><b>Complexity:</b> O(n^2)
+      */
+    private static int solution1(int[] riceBags) {
+        //sort the lst in increasing order
+        Arrays.sort(riceBags);
+
+        int n = riceBags.length;
+        // create a dp array of size n and initialise values with 1
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        //traverse lst from i=0 to end of lst
+        for (int i = 0; i < n; i++) {
+            //traverse lst from j=i-1 to 0
+            for (int j = i - 1; j > -1; j--) {
+                //if following condition passes, then update dp[i] with max(dp[i], dp[j]+1)
+                if (riceBags[j] * riceBags[j] == riceBags[i]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+        }
+
+        Arrays.stream(dp).forEach(System.out::println);
+
+        OptionalInt max = Arrays.stream(dp).max();
+
+
+        //if max(dp) == 1, then return -1
+        if (max.getAsInt() == 1) {
+            return -1;
+        }
+
+        //else return max(dp) which is the max perfect set size
+        return max.getAsInt();
+    }
+
+    /**
+     * as sorting is happening and java use merge sort so complexity of merge sort is
+     * <br/><b>Complexity:</b> O(nlog(n))
+     */
+    private static int solution2(int[] riceBags) {
+        Arrays.sort(riceBags);
+
+        Map<Integer, Integer> riceBagsSetMap = new HashMap<>();
+        for (int i = riceBags.length - 1; i >= 0; i--) {
+            int product = riceBags[i] * riceBags[i];
+            if (riceBagsSetMap.containsKey(product)) {
+                int value = riceBagsSetMap.get(product);
+                riceBagsSetMap.remove(product);
+                riceBagsSetMap.put(riceBags[i], value + 1);
+            } else {
+                riceBagsSetMap.put(riceBags[i], 1);
+            }
+        }
+        System.out.println(riceBagsSetMap);
+        return Collections.max(riceBagsSetMap.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getValue();
     }
 }
--- src/test/java/interviews/amazon/hackerrank2/questions/Question1Test.java
+++ src/test/java/interviews/amazon/hackerrank2/questions/Question1Test.java
@@ -6,6 +6,11 @@ import org.junit.Test;
 public class Question1Test {
     @Test
     public void success_maxSetSize_test_0() {
-        Assert.assertEquals(Question1.maxSetSize(new int[]{3,9,4,2,16}), 3);
+        Assert.assertEquals(Question1.maxSetSize(new int[]{3, 9, 4, 2, 16}), 3);
+    }
+
+    @Test
+    public void success_maxSetSize_test_1() {
+        Assert.assertEquals(Question1.maxSetSize(new int[]{2, 4, 5, 25, 26, 125, 16, 625, 390625}), 4);
     }
 }
