f3b9339939a889d20ac6d79b1c2d3642d2dea736
Yousuf Siddiqui <yousuf.siddiqui@ext.careem.com>
2022-11-25 20:20:19 +0500

    test cases for remotebase question along with resources

---
+++ src/test/java/interviews/remotebase/coding_challenge_hackerrank/questions/Question1Test.java
@@ -0,0 +1,17 @@
+package interviews.remotebase.coding_challenge_hackerrank.questions;
+
+import interviews.remotebase.coding_challenge_hackerrank.question.MaximizeTheValue;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Question1Test {
+    @Test
+    public void success_containingAnyCombination_test_0() {
+        Assert.assertArrayEquals(MaximizeTheValue.rearrange(new int[]{21,34,5,7,9}), new int[]{9,21,5,34,7});
+    }
+
+    @Test
+    public void success_containingAnyCombination_test_1() {
+        Assert.assertArrayEquals(MaximizeTheValue.rearrange(new int[]{1,2,3,4}), new int[]{2,3,1,4});
+    }
+}
