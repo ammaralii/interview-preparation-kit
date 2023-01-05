2fd59f4898d89835b1e97bb320429cbe06bb328b
Magic <ammar.ali@simplifipay.com>
2023-01-05 23:42:53 +0500

    Add some corner cases to test the algorith,

--- src/test/java/interviews/remotebase/coding_challenge_hackerrank/questions/Question1Test.java
+++ src/test/java/interviews/remotebase/coding_challenge_hackerrank/questions/Question1Test.java
@@ -14,4 +14,29 @@ public class Question1Test {
     public void success_rearrange_test_1() {
         Assert.assertArrayEquals(Question1.rearrange(new int[]{1, 2, 3, 4}), new int[]{2, 3, 1, 4});
     }
+
+    @Test
+    public void success_rearrange_test_2() {
+        Assert.assertArrayEquals(Question1.rearrange(new int[]{}), new int[]{});
+    }
+
+    @Test
+    public void success_rearrange_test_3() {
+        Assert.assertArrayEquals(Question1.rearrange(new int[]{1}), new int[]{1});
+    }
+
+    @Test
+    public void success_rearrange_test_4() {
+        Assert.assertArrayEquals(Question1.rearrange(new int[]{1,2}), new int[]{1,2});
+    }
+
+    @Test
+    public void success_rearrange_test_5() {
+        Assert.assertArrayEquals(Question1.rearrange(new int[]{1,2,3}), new int[]{2,3,1});
+    }
+
+    @Test
+    public void success_rearrange_test_6() {
+        Assert.assertArrayEquals(Question1.rearrange(new int[]{1, -2, 3, -4, 5}), new int[]{1, 3, -4, 5, -2});
+    }
 }
