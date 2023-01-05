fc2af949d0660de67aa67404834ff47c8a6b1527
Magic <ammar.ali@simplifipay.com>
2023-01-05 21:30:26 +0500

    Code refacotoring

--- src/test/java/interviews/remotebase/coding_challenge_hackerrank/questions/Question1Test.java
+++ src/test/java/interviews/remotebase/coding_challenge_hackerrank/questions/Question1Test.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 
 public class Question1Test {
     @Test
-    public void success_containingAnyCombination_test_0() {
-        Assert.assertArrayEquals(Question1.rearrange(new int[]{21,34,5,7,9}), new int[]{9,21,5,34,7});
+    public void success_rearrange_test_0() {
+        Assert.assertArrayEquals(Question1.rearrange(new int[]{21, 34, 5, 7, 9}), new int[]{9, 21, 5, 34, 7});
     }
 
     @Test
-    public void success_containingAnyCombination_test_1() {
-        Assert.assertArrayEquals(Question1.rearrange(new int[]{1,2,3,4}), new int[]{2,3,1,4});
+    public void success_rearrange_test_1() {
+        Assert.assertArrayEquals(Question1.rearrange(new int[]{1, 2, 3, 4}), new int[]{2, 3, 1, 4});
     }
 }
